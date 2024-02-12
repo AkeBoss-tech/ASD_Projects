5658fd7494f77b390d746f82defb41e69ce358b2
Akash Dubey <69588353+AkeBoss-tech@users.noreply.github.com>
2024-02-12 13:23:38 -0500

    initial commit

--- Main.java
+++ Main.java
@@ -6,90 +6,83 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class Point {
-    double x, y, z;
+class Parameter {
+    double B, R, C; // Volts, Ohms, Micro Farads
+    double time_start, time_end; // Micro Seconds
 
-    public Point(double x, double y, double z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    public Parameter(double B, double R, double C, double time_start, double time_end) {
+        this.B = B;
+        this.R = R;
+        this.C = C;
+        this.time_start = time_start;
+        this.time_end = time_end;
     }
 
-    public Point(String x, String y, String z) {
-        this.x = Double.parseDouble(x);
-        this.y = Double.parseDouble(y);
-        this.z = Double.parseDouble(z);
+    public Parameter(String B, String R, String C, String time_start, String time_end) {
+        this.B = Double.parseDouble(B);
+        this.R = Double.parseDouble(R);
+        this.C = Double.parseDouble(C);
+        this.time_start = Double.parseDouble(time_start);
+        this.time_end = Double.parseDouble(time_end);
     }
 
-    public double distance(Point p) {
-        return Math.sqrt(Math.pow(p.x - x, 2) + Math.pow(p.y - y, 2) + Math.pow(p.z - z, 2));
-    }
-
-    public Point center(Point p) {
-        return new Point((x + p.x) / 2, (y + p.y) / 2, (z + p.z) / 2);
-    }
-
-    public Point add(Point p) {
-        return new Point(x + p.x, y + p.y, z + p.z);
+    public double getVTime(double time) {
+        return B * (1 - Math.exp(-time / R / C));
     }
 
     @Override
     public String toString() {
-        return x + " " + y + " " + z;
+        return "B: " + B + ", R: " + R + ", C: " + C + ", time_start: " + time_start + ", time_end: " + time_end;
     }
 
-    public Point divide(int n) {
-        return new Point(x / n, y / n, z / n);
+    public double getB() {
+        return B;
+    }
+
+    public double getR() {
+        return R;
+    }
+
+    public double getC() {
+        return C;
+    }
+
+    public double getTimeStart() {
+        return time_start;
+    }
+
+    public double getTimeEnd() {
+        return time_end;
     }
 }
 
 public class Main {
+    private static final double TIME_CONSTANT = 0.1;
+
     public static void main(String args[]) throws IOException {
         BufferedReader reader = null;
         BufferedWriter output = null;
 
-        ArrayList<Point> points = new ArrayList<Point>();
+        ArrayList<Parameter> parameters = new ArrayList<Parameter>();
 
         try {
-            reader = new BufferedReader(new FileReader("points.txt"));
+            reader = new BufferedReader(new FileReader("params.txt"));
             output = new BufferedWriter(new FileWriter("output.txt"));
 
             String line;
             while ((line = reader.readLine()) != null) {
                 // Split the line into individual coordinates
                 String[] coordinates = line.split(" ");
-                Point point = new Point(coordinates[0], coordinates[1], coordinates[2]);
-                points.add(point);
+                Parameter point = new Parameter(coordinates[0], coordinates[1], coordinates[2], coordinates[3], coordinates[4]);
+                parameters.add(point);
             }
 
-            printPoints(points);
-            printMagnitudes(points);
+            printParameters(parameters);
 
-            writeTextToFile(printMagnitudes(points), output);
+            writeTextToFile(getTimes(parameters), output);
 
-            System.out.println("Center: " + calculateCenter(points));
-            
-            Scanner scanner = new Scanner(System.in);
-
-            // Allow users to add points
-            System.out.println("Do you want to add more points? (y/n): ");
-            String userInput = scanner.next().toLowerCase();
-            boolean addMorePoints = userInput.equals("y");
-            while (addMorePoints) {
-                Point newPoint = getUserInput(scanner);
-                points.add(newPoint);
-
-                System.out.println("Do you want to add more points? (y/n): ");
-                userInput = scanner.next().toLowerCase();
-                addMorePoints = userInput.equals("y");
-            }
-
-            // Perform operations on points
-            System.out.println("Center: " + calculateCenter(points));
-
-            // Write points to the output file
-            writeToFile(points);
-            writeTextToFile(printMagnitudes(points), output);
+            // Write parameters to the output file
+            writeTextToFile(getTimes(parameters), output);
         } finally {
             if (reader != null) {
                 reader.close();
@@ -100,37 +93,7 @@ public class Main {
         }
     }
 
-    private static Point calculateCenter(ArrayList<Point> points) {
-        Point center = new Point(0,0,0);
-        for (Point p : points) {
-            center = center.add(p);
-        }
-
-        center = center.divide(points.size());
-        return center;
-    }
-
-    private static Point getUserInput(Scanner scanner) {
-        System.out.println("Enter the x coordinate of the point: ");
-        double x = scanner.nextDouble();
-        System.out.println("Enter the y coordinate of the point: ");
-        double y = scanner.nextDouble();
-        System.out.println("Enter the z coordinate of the point: ");
-        double z = scanner.nextDouble();
-        return new Point(x, y, z);
-    }
-
-    private static void writeToFile(ArrayList<Point> points) throws IOException {
-        BufferedWriter output = new BufferedWriter(new FileWriter("points.txt"));
-        for (Point p : points) {
-            output.write(p.toString());
-            output.newLine();
-        }
-        output.close();
-    }
-
     public static void writeTextToFile(String text, BufferedWriter file) throws IOException {
-        
         String[] printer = text.split("\n");
         for (String line : printer) {
             file.write(line);
@@ -138,21 +101,22 @@ public class Main {
         }
     }
 
-    private static String printPoints(ArrayList<Point> points) {
+    private static String printParameters(ArrayList<Parameter> parameters) {
         String output = "";
-        for (Point p : points) {
+        for (Parameter p : parameters) {
             output += p.toString() + '\n';
         }
-        System.out.println("Points: ");
+        System.out.println("Parameters: ");
         System.out.println(output);
         return output;
     }
 
-    private static String printMagnitudes(ArrayList<Point> points) {
+    private static String getTimes(ArrayList<Parameter> parameters) {
         String output = "";
-        for (int i = 0; i < points.size(); i++) {
-            for (int j = i + 1; j < points.size(); j++) {
-                output += "Distance between " + points.get(i) + " and " + points.get(j) + " is " + points.get(i).distance(points.get(j)) + '\n';
+        for (int i = 0; i < parameters.size(); i++) {
+            output += "Parameter " + (i + 1) + ": ";
+            for (double time = parameters.get(i).getTimeStart(); time <= parameters.get(i).getTimeEnd(); time += TIME_CONSTANT) {
+                output += parameters.get(i).getVTime(time) + " ";
             }
         }
         System.out.println(output);
